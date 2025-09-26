8b1f2f0ed100df5167a885b2b29542960658c395
Nguyễn Lê Tấn Pháp <letanphap6543@gmail.com>
2025-09-27 03:06:40 +0700

    DoctorRepository.java

---
+++ app/src/main/java/com/project/backend/repo/DoctorRepository.java
@@ -0,0 +1,12 @@
+package com.project.backend.repo;
+
+import com.project.backend.models.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Optional<Doctor> findByEmail(String email);
+}
