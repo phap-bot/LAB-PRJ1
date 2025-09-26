e7d0453e3d480ffc34a3396f69b5b283ba8508e3
Nguyễn Lê Tấn Pháp <letanphap6543@gmail.com>
2025-09-27 02:22:18 +0700

    PatientRepository.java

---
+++ app/src/main/java/com/project/backend/repo/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.project.backend.repo;
+
+import com.project.backend.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {}
